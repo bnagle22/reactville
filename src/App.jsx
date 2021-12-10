826617aecff58f4a914360cecc43a31337687508
bnagle22 <naglebenjamin@gmail.com>
2021-12-10 16:48:08 -0500

    add super market

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import './styles/App.css'
-
-// Components
+import { Route, Routes } from 'react-router-dom'
 import Landing from './Components/Landing/Landing'
+import BurgerShop from './Components/BurgerShop/BurgerShop'
+import Nav from './Components/Nav/Nav'
+
 
 const App = () => {
   return (
+    <>
+    <Nav />
     <main>
-      <Landing />
+      {/* //add route paths */}
+      <Routes>
+        <Route path="/"
+          element={<Landing />}
+        />
+        <Route path="/burgers"
+          element={<BurgerShop />}
+        />
+      </Routes>
     </main>
-  )
-}
+    </>
+  )}
 
 export default App
--- src/Components/SuperMarket/DisplayProducts.jsx
+++ src/Components/SuperMarket/DisplayProducts.jsx
@@ -1,12 +1,46 @@
 import React from 'react'
+import Product from './Product'
 
-const DisplayProducts = () => {
-  
+const DisplayProducts = (props) => {
+
+  const selectedProducts = props.products.map((product, idx) => {
+    return product.category === props.productCategory &&
+      <Product
+        key={idx}
+        product={product}
+        addToCart={props.addToCart}
+      />
+  })
+
   return (
     <div className="product-list">
-      Use the map function and Product component here
+      {selectedProducts}
     </div>
   )
 }
 
 export default DisplayProducts
+
+// import React from 'react'
+// import Product from './Product'
+// import { products } from '../../data/market-data'
+
+
+// const DisplayProducts = (props) => {
+  
+
+//   return (
+//     <div className="product-list">
+//         {props.products.map((product, idx) => (
+//           product.category && 
+//           <Product
+//             key={idx}
+//             product={product}
+
+//           />
+//         ))}
+//     </div>
+//   )
+// }
+
+// export default DisplayProducts
